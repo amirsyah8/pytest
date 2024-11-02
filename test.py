7753d223053fa1729cb0d5281cce012dccb67983
unknown <amirsyah87@gmail.com>
2024-11-02 08:41:44 +0800

    not in

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
-username = input("Enter a username : ")
-if len(username)>12:
-    print("tidak boleh lebih 12")
-elif not username.find(" ") == -1:
-    print("Tak boleh ada space")
-else:
-    print(f"welcome {username}")
+while True:
+    try:
+        username = input("Enter an email: ")
+        if len(username) > 12:
+            print("tidak boleh lebih 12")
+        elif " " in username:
+            print("Tak boleh ada space")
+        elif "@" not in username:
+            print("Must contain @")
+        else:
+            print(f"Welcome {username}")
+            break
+    except:
+        print("An error occurred, please try again.")
+
+
